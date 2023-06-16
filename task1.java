968bcb768717b2e0fb6ab654cffafa2c1b483e82
Ivan <ivanzhuravlev9051995@gmail.com>
2023-06-16 20:02:38 +0300

    Added task 1,2

--- task1.java
+++ task1.java
@@ -1,35 +1,30 @@
-// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
-// 4) (дополнительное задание) Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса, например, 2? + ?5 = 69. Требуется восстановить выражение до верного равенства. Предложить хотя бы одно решение или сообщить, что его нет.
-// Под знаком вопроса - всегда одинаковая ЦИФРА.
-// Введите уравнение: ?? + ?? = 44
-// Решение: 22 + 22 = 44
+// 1) Дана строка sql-запроса "select * from students WHERE ". Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+// Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Пример данной строки {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+// Вывод: select * from students WHERE name=Ivanov AND country=Russia AND city=Moscow
 
-import java.util.Scanner;
 
 public class task1 {
     public static void main(String[] args) {
-        // 1ое задание
-        int a = 1;
-        int count = 0;
-        int count1 = 1;
-        System.out.println("Введите число: ");
-        Scanner sc = new Scanner(System.in);
-        int name = sc.nextInt();
-        System.out.println("1 - Треугольное число, 2 - факториал:");
-        int sum = sc.nextInt();
-        while (a <= name) {
-            if (sum == 1) {
-                count += a;
-                a += 1;
+        String stroka = "name:Ivanov ,country:Russia ,city:Moscow ,age:null";
+        String[] phrase = stroka.split(",");
+                for (String i : phrase) {
+                    if (i.contains("null")) {
+                        i = i.replace(i,"");
+                    }
+        StringBuilder s = new StringBuilder();
+            if (i != "") {
+                s.append(i);
+                if (i.contains(" ")) {
+                    i = i.replace(i,"AND ");
+                    s.append(i);
+                    s.toString();
+                }
             }
-            else {
-                count1 *= a;
-                a += 1;
-                count = count1;
-            }            
-        }
-        System.out.println(count);
-        sc.close();
+        System.out.print("select * from students WHERE ");
+        System.out.print(s);
     }
-    
 }
+}
+// Прошу прощения, но я вообще не понял как это сделать, весь день убил на вот это((
--- task2.java
+++ task2.java
@@ -1,17 +1,28 @@
-// 2) Вывести все простые числа от 1 до 1000
+// 2) Реализуйте алгоритм сортировки пузырьком числового массива, 
+// результат после каждой итерации запишите в лог-файл.
+// 1 3 4 2
+// 1 3 2 4
+// 1 2 3 4
+
+import java.util.Arrays;
 
 public class task2 {
     public static void main(String[] args) {
-        for ( int i = 2; i < 1000; ++i) {
-            int count = 0;
-            for (int j = 2; j <= i && count < 2; ++j) {
-                if (i % j == 0){
-                    ++count;
+        int[] arr = {7, 4, 9, 1, 2, 4, 0, 5, 23};
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < arr.length-1; i++) {
+                if (arr[i] > arr[i+1]) {
+                    isSorted = false;
+
+                    buf = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = buf;
                 }
             }
-            if (count < 2) {
-                System.out.println(i);
-            }
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
